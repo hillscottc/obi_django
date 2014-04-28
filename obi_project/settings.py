423b99008ab2c5a219955dba88c93641a6ff4a52
Scott Hill <scott289@gmail.com>
2014-04-28 16:25:31 -0700

    settings prob

--- obi_project/settings.py
+++ obi_project/settings.py
@@ -60,22 +60,7 @@ TEMPLATE_DIRS = [
     os.path.join(BASE_DIR, 'obi_app', 'templates'),
 ]
 
-SITE_NAME = "Obi Site"
-
-PROJECT_APPS = ('obi_app', )
-
-JENKINS_TASKS = (
-    'django_jenkins.tasks.run_pylint',
-    'django_jenkins.tasks.with_coverage',
-    # 'django_jenkins.tasks.django_tests',   # select one django or
-    # #'django_jenkins.tasks.dir_tests'      # directory tests discovery
-    'django_jenkins.tasks.run_pep8',
-    # 'django_jenkins.tasks.run_pyflakes',
-    # 'django_jenkins.tasks.run_jslint',
-    # 'django_jenkins.tasks.run_csslint',
-    'django_jenkins.tasks.run_sloccount',
-    'django_jenkins.tasks.run_graphmodels',
-)
+# SITE_NAME = "Obi Site"
 
 
 ## In the heroku environ this will be set
